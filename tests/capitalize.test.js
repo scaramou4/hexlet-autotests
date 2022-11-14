4fd7d3855773e320a92bfed9055d53d4ad56e176
Victor Nosov <scaramou4@gmail.com>
2022-11-14 14:55:15 +0300

    add assertion use

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,12 +1,8 @@
 
 import { capitalize } from '../src/capitalize.js';
+import { strict as assert } from 'node:assert';
 
-if (capitalize('hello') !== 'Hello') {
-    throw new Error('Функция работает неверно!');
-}
-
-if (capitalize('') !== '') {
-    throw new Error('Функция работает неверно!');
-}
+assert.deepEqual(capitalize('hello'), 'Hello');
+assert.deepEqual(capitalize(''), '');
 
 console.log('Все тесты пройдены!');
